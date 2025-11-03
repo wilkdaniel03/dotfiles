e3de990c325722b16e8434af33515e9c000e151f
Daniel Wilk <wilkdaniel03@gmail.com>
2025-11-03 09:57:06 +0100

    [nvim] modyfying nvim

---
+++ nvim/after/plugin/cmp.lua
@@ -0,0 +1,86 @@
+local cmp = require('cmp')
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+			vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+
+			-- For `mini.snippets` users:
+			-- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
+			insert({ body = args.body }) -- Insert at cursor
+			-- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
+			-- require("cmp.config").set_onetime({ sources = {} })
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		-- { name = 'vsnip' }, -- For vsnip users.
+		-- { name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require('lspconfig')['pyright'].setup {
+	capabilities = capabilities,
+	before_init = function(_, config)
+		config.settings.python.pythonPath = "./.venv/bin/python3"
+	end,
+}
+
+require('lspconfig')['clangd'].setup {
+	capabilities = capabilities
+}
+
+require('lspconfig')['ts_ls'].setup {
+	capabilities = capabilities
+}
+
+require('lspconfig')['bashls'].setup {
+	capabilities = capabilities
+}
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf, silent = true }
+
+		vim.keymap.set("n","<space>k",vim.diagnostic.open_float,opts)
+		vim.keymap.set("n","gd",vim.lsp.buf.definition,opts)
+		vim.keymap.set("n","gD",vim.lsp.buf.declaration,opts)
+	end,
+})
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,45 +1,72 @@
-vim.opt['number'] = true
-vim.opt['relativenumber'] = true
-vim.opt['swapfile'] = false
-vim.opt['tabstop'] = 4
-vim.opt['shiftwidth'] = 4
-vim.opt['expandtab'] = true
-
-local opts = { silent = true, noremap = true }
-vim.keymap.set('n','<SPACE>w','<CMD>w<CR>',opts)
-vim.keymap.set('n','<SPACE>q','<CMD>q!<CR>',opts)
-vim.keymap.set('n','<SPACE>e','<CMD>Explore<CR>',opts)
-vim.keymap.set('i','jk','<ESC>',opts)
-
-require('nvim-treesitter.configs').setup {
-    auto_install = true,
-
-    highlight = {
-        enable = true
-    }
+opts = {
+	number = true,
+	relativenumber = true,
+	tabstop = 4,
+	shiftwidth = 4,
+	scrolloff = 8
 }
 
-vim.cmd('colorscheme catppuccin-mocha')
+for k,v in pairs(opts) do
+	vim.opt[k] = v
+end
+
+require'nvim-treesitter.configs'.setup {
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
+}
+
+require'nvim-autopairs'.setup{}
+require'oil'.setup{
+	view_options = {
+		show_hidden = true
+	}
+}
+require'lsp_lines'.setup()
+
+vim.diagnostic.config({
+  virtual_text = true
+})
+
+vim.cmd("colorscheme carbonfox")
+
+vim.keymap.set('i','jk','<ESC>')
+vim.keymap.set('n','<space>w','<CMD>w<CR>')
+vim.keymap.set('n','<space>q','<CMD>q!<CR>')
+vim.keymap.set('n','<space>e','<CMD>Oil<CR>')
+vim.keymap.set('n','<s-t>','<CMD>tabnew<CR>')
+vim.keymap.set('n','<s-n>','<CMD>tabnext<CR>')
+vim.keymap.set('n','<s-m>','<CMD>tabprevious<CR>')
 
 local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use 'nvim-treesitter/nvim-treesitter'
-    use 'catppuccin/nvim'
+	use 'wbthomason/packer.nvim'
+	use 'nvim-treesitter/nvim-treesitter'
+	use 'windwp/nvim-autopairs'
+	use 'EdenEast/nightfox.nvim'
+	use 'stevearc/oil.nvim'
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/nvim-cmp'
+	use 'ErichDonGubler/lsp_lines.nvim'
+	use 'numToStr/Comment.nvim'
 
-    if packer_bootstrap then
-        require('packer').sync()
-    end
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
